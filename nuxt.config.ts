c6f8bff27ac055a89fd5ebc2bab20d411c041a96
piDev (Sue Holder) <sue@designdevelophost.co.uk>
2025-09-24 22:26:43 +0100

    force ssr and .output publish folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: "netlify",
+    output: {
+      dir: ".output",
+      publicDir: ".output/public",
+      serverDir: ".output/server",
+    },
+  },
 
   css: ["~/assets/css/main.css"],
 
   modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
-  nitro: {
-    preset: "netlify", // 👈 ensures SSR is packaged for Netlify Functions
-  },
 });
