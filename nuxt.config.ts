eaf4e0961a7228ae6814a1a315147cb044bb07b5
piDev (Sue Holder) <sue@designdevelophost.co.uk>
2025-09-25 09:28:01 +0100

    FIX: remove _redirects and netlify.toml remove force publish folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  ssr: true,
   nitro: {
     preset: "netlify",
-    output: {
-      dir: ".output",
-      publicDir: ".output/public",
-      serverDir: ".output/server",
-    },
   },
 
   css: ["~/assets/css/main.css"],
